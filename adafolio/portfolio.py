cf901ad20945dabec294d7c6347c63221ca79a32
Robert Roose <robert@shivercube.com>
2022-02-19 13:05:49 +0100

    Adde Portfolio class

--- adafolio/portfolio.py
+++ adafolio/portfolio.py
@@ -6,10 +6,17 @@ from adafolio.Pool import Pool
 ADAFOLIO_PORTFOLIO_URL = "https://adafolio.com/portfolio/download/"
 
 
+class Portfolio:
+    def __init__(self, portfolio):
+        self._data = requests.get(ADAFOLIO_PORTFOLIO_URL + portfolio).json()
+
+    def get_members(self):
+        return [
+            Pool(pool["pool_id"].strip(), pool["ticker"].upper().strip())
+            for pool in self._data["pools"]
+        ]
+
+
 def get_members(portfolio):
     """Lists all the members of an adafolio portfolio."""
-    return [
-        Pool(pool["pool_id"].strip(), pool["ticker"].upper().strip())
-        for pool in requests.get(
-            ADAFOLIO_PORTFOLIO_URL + portfolio).json()["pools"]
-    ]
+    return Portfolio(portfolio).get_members()
